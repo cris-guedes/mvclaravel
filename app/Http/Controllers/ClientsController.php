d36fdda57103e6093e31978b48cd22e9bfbb7860
cris-guedes <cristianguedes136@gmail.com>
2021-12-19 18:07:02 -0300

    controller clients and employees create

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -7,30 +7,41 @@ use Illuminate\Support\Facades\DB;
 
 class ClientsController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
         return view('clientsPages.index');
     }
-    public function create(Request $request)
+    public function create()
     {
 
         return view('clientsPages.create');
     }
     public function createAction(Request $request)
     {
+        $nome =$request->input('nome');
+        $telefone =$request->input('telefone');
+        $cpf =$request->input('cpf');
+        DB::insert('insert into clients (nome,telefone,cpf) values (?,?,?)',[$nome,$telefone,$cpf]);
 
     }
-    public function update(Request $request)
+    public function update()
     {
 
         return view('clientsPages.update');
     }
     public function updateAction(Request $request)
     {
+        $nome =$request->input('nome');
+        $telefone =$request->input('telefone');
+        $cpf =$request->input('cpf');
+        $id=$request->input('id');
+        DB::update('update clients set nome = ?, telefone = ?, cpf = ? where id = ?',[$nome,$telefone,$cpf,$id]);
 
     }
     public function destroy(Request $request)
     {
+        $id=$request->input('id');
+        DB::delete('delete from clients where id = ?',[$id]);
 
 
     }
--- app/Http/Controllers/EmployeesController.php
+++ app/Http/Controllers/EmployeesController.php
@@ -3,8 +3,50 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class EmployeesController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('employeesPages.index');
+    }
+    public function create()
+    {
+
+        return view('employeesPages.create');
+    }
+    public function createAction(Request $request){
+     $nome =$request->input('nome');
+     $telefone =$request->input('telefone');
+     $cpf =$request->input('cpf');
+     $cargo =$request->input('cargo');
+    DB::insert('insert into employees (nome,telefone,cpf,cargo) values (?,?,?,?)',[$nome,$telefone,$cpf,$cargo]);
+
+
+
+    }
+    public function update()
+    {
+
+        return view('employeesPages.update');
+    }
+    public function updateAction(Request $request){
+    $nome =$request->input('nome');
+    $telefone =$request->input('telefone');
+    $cpf =$request->input('cpf');
+    $cargo =$request->input('cargo');
+    $id=$request->input('id');
+    DB::update('update employees set nome = ?, telefone = ?, cpf = ?, cargo = ? where id = ?',[$nome,$telefone,$cpf,$cargo,$id]);
+
+
+
+    }
+    public function destroy(Request $request)
+    {
+        $id=$request->input('id');
+        DB::delete('delete from employees where id = ?',[$id]);
+
+
+    }
 }
